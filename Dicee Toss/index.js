8d289c693e93c7232d279aee632ea24a54f55588
Spandan2405 <121621850+Spandan2405@users.noreply.github.com>
2024-06-27 16:46:26 +0530

    Update index.js

--- Dicee Toss/index.js
+++ Dicee Toss/index.js
@@ -1,70 +1,24 @@
 function randomNumber(){
-    var x1 = Math.random();  // COULD HAVE USED FLOOR AND ADDED 1 
-    x1 = x1 * 6  ;
-    var y1 = Math.round(x1);
-    return y1;
+    return Math.ceil(Math.random() * 6);
 }
 
 // GENERATE 2 RN AND ADD THEM TO THE IMG SRC LINK 
-
-function d1(){
-    var x = randomNumber();
-    if(x == 1){
-        document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-    }
-    else if(x == 2){
-        document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-    }
-    else if(x == 3){
-        document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-    }
-    else if(x == 4){
-        document.querySelector(".img1").setAttribute("src","./images/dice4.png");
-    }
-    else if(x == 5){
-        document.querySelector(".img1").setAttribute("src","./images/dice5.png");
-    }
-    else if(x == 6){
-        document.querySelector(".img1").setAttribute("src","./images/dice6.png");
-    }
-    // console.log(x); 
-    return x;
-}
-function d2(){
-    var y = randomNumber();
-    if(y == 1){
-        document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-    }
-    else if(y == 2){
-        document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-    }
-    else if(y == 3){
-        document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-    }
-    else if(y == 4){
-        document.querySelector(".img2").setAttribute("src","./images/dice4.png");
-    }
-    else if(y == 5){
-        document.querySelector(".img2").setAttribute("src","./images/dice5.png");
-    }
-    else if(y == 6){
-        document.querySelector(".img2").setAttribute("src","./images/dice6.png");
-    }
-    // console.log(y);
-    return y;
-    
+function diceImage(diceImage, diceNumber){
+    document.querySelector(`.img${diceImage}`).setAttribute("src",`./images/dice${diceNumber}.png`);
 }
 
 function output(){
-    var x = d1();
-    var y = d2();
-    console.log(x);
-    console.log(y);
+    var dice1 = randomNumber();
+    var dice2 = randomNumber();
+    diceImage(1,dice1);
+    diceImage(2,dice2);
+    console.log(dice1);
+    console.log(dice2);
 
-if(x > y){
+if(dice1 > dice2){
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
-else if(x < y){
+else if(dice1 < dice2){
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 else {
